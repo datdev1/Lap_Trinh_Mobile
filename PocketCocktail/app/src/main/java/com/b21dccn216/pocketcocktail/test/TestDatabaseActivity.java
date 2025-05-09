6f21ed09bdadbffac25c9cda422470caed9c0707
MonHauVD <tranvietdung121@gmail.com>
2025-05-10 00:35:53 +0700

    Test database

---
+++ PocketCocktail/app/src/main/java/com/b21dccn216/pocketcocktail/test/TestDatabaseActivity.java
@@ -0,0 +1,26 @@
+package com.b21dccn216.pocketcocktail.test;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.b21dccn216.pocketcocktail.R;
+
+public class TestDatabaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_test_database);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
