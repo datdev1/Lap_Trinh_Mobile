9ca238d0b223d066a0bbd1018c28f8524f4ab8bd
Dat Tran <datforitwork@gmail.com>
2025-05-09 09:34:28 +0700

    base MVP setup

--- PocketCocktail/app/src/main/java/com/b21dccn216/pocketcocktail/Base/BaseApp.java
+++
@@ -1,9 +0,0 @@
-package com.b21dccn216.pocketcocktail.Base;
-
-import android.app.Application;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class BaseApp extends AppCompatActivity implements IContractor.IView{
-
-}
--- PocketCocktail/app/src/main/java/com/b21dccn216/pocketcocktail/Base/BasePresenter.java
+++
@@ -1,5 +0,0 @@
-package com.b21dccn216.pocketcocktail.Base;
-
-public class BasePresenter<V extends BaseApp>  implements IContractor.IPresenter{
-
-}
