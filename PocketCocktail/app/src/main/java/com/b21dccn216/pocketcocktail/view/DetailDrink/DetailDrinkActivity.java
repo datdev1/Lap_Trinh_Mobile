616d0e3372a1691225f7e5833b67874a36dc013f
Dưc Loc <tranducloc2010@gmail.com>
2025-05-10 15:23:01 +0700

    Update DetailDrinkActivity version_01

--- PocketCocktail/app/src/main/java/com/b21dccn216/pocketcocktail/view/DetailDrink/DetailDrinkActivity.java
+++ PocketCocktail/app/src/main/java/com/b21dccn216/pocketcocktail/view/DetailDrink/DetailDrinkActivity.java
@@ -1,26 +1,51 @@
 package com.b21dccn216.pocketcocktail.view.DetailDrink;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.b21dccn216.pocketcocktail.R;
 
+import com.b21dccn216.pocketcocktail.model.Drink;
+import com.b21dccn216.pocketcocktail.model.Ingredient;
+import com.b21dccn216.pocketcocktail.dao.DrinkDAO;
+import com.b21dccn216.pocketcocktail.dao.IngredientDAO;
+
+
+
 public class DetailDrinkActivity extends AppCompatActivity {
+    private ImageView drinkImage;
+    private ImageButton backButton, favoriteButton, shareButton;
+    private TextView badge, drinkTitle, drinkDescription;
+    private LinearLayout ingredientsLayout, instructionsLayout;
+    private DrinkDAO drinkDAO;
+    private IngredientDAO ingredientDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_detail_drink);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Ánh xạ
+        drinkImage = findViewById(R.id.drink_image);
+        backButton = findViewById(R.id.back_button);
+        favoriteButton = findViewById(R.id.favorite_button);
+        shareButton = findViewById(R.id.share_button);
+        badge = findViewById(R.id.badge);
+        drinkTitle = findViewById(R.id.drink_title);
+        drinkDescription = findViewById(R.id.drink_description);
+        ingredientsLayout = findViewById(R.id.ingredients_layout);
+        instructionsLayout = findViewById(R.id.instructions_layout);
+
+        // DAO
+        drinkDAO = new DrinkDAO();
+        ingredientDAO = new IngredientDAO();
+
+        
     }
 }
